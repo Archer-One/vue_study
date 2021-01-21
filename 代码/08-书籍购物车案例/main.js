ba7b374f9696d07f20dcfde3df6da58c7b32ea3f
Archer-One <1293150622@qq.com>
2021-01-21 13:32:16 +0800

    1.21

--- 代码/08-书籍购物车案例/main.js
+++ 代码/08-书籍购物车案例/main.js
@@ -50,10 +50,20 @@ const app = new Vue({
   computed:{
     totalPrice(){
       let sum = 0;
-      for(let i=0; i<this.books.length; i++){
-        console.log('--------'+this.books[i].price+'-------'+this.books[i].count);
+      // for(let i=0; i<this.books.length; i++){
+      //   console.log('--------'+this.books[i].price+'-------'+this.books[i].count);
+      //   sum += this.books[i].price * this.books[i].count;
+      // }
+
+
+      for(let i in this.books){
         sum += this.books[i].price * this.books[i].count;
       }
+
+      for(let item of this.books){
+        sum += item.count * item.price;
+      }
+
       return sum;
     }
   },
@@ -64,3 +74,38 @@ const app = new Vue({
   }
 
 })
+
+//filter
+//filter中的回调函数有一个要求：返回值必须是Boolean值
+//true: 当返true时，函数内部会自动将这次回调的n加入到新的数组中
+//false：当返回false时，函数内部会过滤掉这次的n
+const  nums = [10, 20, 30, 111, 225, 250, 330]
+
+let newNums = []
+newNums = nums.filter(function (n){
+  if(n>=100)
+    return true;
+  else
+    return false;
+})
+
+newNums.map(function (n){
+  return n*2;
+})
+
+//reduce作用是对数组中所有元素进行汇总
+newNums.reduce(function (preValue,n){
+  return preValue+n
+},0)
+
+total = nums.filter(function (n){
+  return n>100
+}).map(function (n){
+  return n*2
+}).reduce(function (preValue,n){
+  return preValue + n
+},0)
+
+total = nums.filter(n => n>100).map(n => n*2).reduce((prev,n) => prev + n);
+
+
